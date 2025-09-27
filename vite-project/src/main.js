e0a8e70873605200ace8e70ab22f28e5503e49c9
Tomas <tomaspaulenas@hotmail.com>
2025-09-27 22:05:33 +0200

    First functional version

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,19 +1,21 @@
 import './style.css'
+
 const form = document.querySelector('#form')
 const input = document.querySelector('#text')
 const list = document.querySelector('#list');
+let tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+function syncFromDOM(){
+  tasks = [...document.querySelectorAll('#list li span')]
+.map(s => ({text: s.textContent}));
+localStorage.setItem('tasks',JSON.stringify(tasks));
+}
 
-console.log(form, input , list);
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
-
-const task = input.value.trim();
-if(!task)return;
-
+if(tasks.length !==0)
+  tasks.forEach(element => {
+      
 const li = document.createElement('li');
 li.innerHTML =` 
-<span>${task}</span>
+<span>${element.text}</span>
 <button class="edit">Edit</button>
 <button class="delete">Delete</button>
 `
@@ -21,6 +23,9 @@ li.innerHTML =`
 const deleteBtn = li.querySelector('.delete')
 deleteBtn.addEventListener('click', () => {
   li.remove();
+    syncFromDOM()
+
+
 })
 
 let span = li.querySelector('span')
@@ -41,6 +46,7 @@ editBtn.addEventListener('click', () => {
       if (e.key === 'Escape')  {
         inputEdit.replaceWith(span)
         editBtn.textContent = 'edit';
+        
       }
     })
   } else{
@@ -51,12 +57,78 @@ editBtn.addEventListener('click', () => {
     currentlyEditing.replaceWith(newSpan);
     span = newSpan
     editBtn.textContent = 'edit'
+    syncFromDOM()
+  }
+
+  });
+list.appendChild(li);
+});
+syncFromDOM()
+
+console.log(form, input , list);
+
+form.addEventListener('submit', (event) => {  
+  event.preventDefault()
+
+const task = input.value.trim();
+if(!task)return;
+
+tasks.push({text:task})
+console.log(tasks)
+localStorage.setItem('tasks', JSON.stringify(tasks));
+
+const li = document.createElement('li');
+li.innerHTML =` 
+<span>${task}</span>
+<button class="edit">Edit</button>
+<button class="delete">Delete</button>
+`
+
+const deleteBtn = li.querySelector('.delete')
+deleteBtn.addEventListener('click', () => {
+  li.remove();
+
+  syncFromDOM()
+})
+
+let span = li.querySelector('span')
+const editBtn = li.querySelector('.edit')
+
+editBtn.addEventListener('click', () => {
+  const currentlyEditing = li.querySelector('input');
+
+  if(!currentlyEditing){
+    const inputEdit = document.createElement('input')
+    inputEdit.value = span.textContent;
+    span.replaceWith(inputEdit) 
+    inputEdit.focus()
+    editBtn.textContent = 'save';
+
+    inputEdit.addEventListener('keydown', (e) =>{
+      if (e.key === 'Enter') editBtn.click()
+      if (e.key === 'Escape')  {
+        inputEdit.replaceWith(span)
+        editBtn.textContent = 'edit';
+
+        
+      }
+    })
+  } else{
+    const newtext = currentlyEditing.value.trim()
+    if(!newtext) return 
+    const newSpan = document.createElement('span')
+    newSpan.textContent = newtext
+    currentlyEditing.replaceWith(newSpan);
+    span = newSpan
+    editBtn.textContent = 'edit'
+    syncFromDOM();
   }
 })
 
 list.appendChild(li);
+syncFromDOM()
+
 
 input.value = '';
 input.focus();
-})  
-
+});
